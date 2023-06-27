044e14d045653b4ebb16209c475ab506e957723e
Cha-er-si <vpcharles31@gmail.com>
2023-06-27 13:42:27 +0800

    Project Reducer

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: 1, title: "Project 1", content: "lorem ipsum" },
+    { id: 2, title: "Project 2", content: "lorem ipsum" },
+    { id: 3, title: "Project 3", content: "lorem ipsum" },
+  ],
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
